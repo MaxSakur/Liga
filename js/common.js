d52a095f301d7ed7eccab847b2d964f9723b046d
Max Kaliakin <max.kaliakin@gmail.com>
2018-09-13 11:28:26 -0700

    Morning fixes

--- js/common.js
+++ js/common.js
@@ -15,6 +15,22 @@ $(document).ready(function(){
 
   AOS.init();
 
+  var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:true,
+    nav:true,
+    lazyLoad:true,
+    // margin: 250,
+    items:3
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
   
 
   
